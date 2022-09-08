5e6d55326ebfb2fba3ff919e7a07601484c92e58
nghia191 <mai.nam.nghia.bunbu@gmail.com>
2022-09-08 14:30:47 +0700

    add date

--- models/product.js
+++ models/product.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-    id: Schema.Types.ObjectId,
     name: { type: String, require: true },
     description: { type: String, require: true },
     price: { type: Number, require: true },
     images: { type: String, require: false },
+}, {
+    timestamps: true,
 })
 
 module.exports = mongoose.model('ProductSchema', ProductSchema);
